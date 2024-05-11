73504740805b965f4696441a8c2b57cef925b221
bbricard <baptiste.bricard@etudiant.univ-rennes1.fr>
2024-05-12 01:14:37 +0200

    création dossiers

--- LDB/ldb.js
+++ LDB/ldb.js
@@ -1,3 +1,35 @@
+const fs = require('fs'); //FileSystem
+const prompt = require('prompt-sync')();
+
+// const name = prompt('What is your name?');
+// console.log(`Hey there ${name}`);
+
 class LDB{
-    
+
+    constructor(DBname,motherPath){
+        this.DBname = DBname;
+        this.path = motherPath;
+        
+        if (!fs.existsSync(`../${motherPath}`)){
+            var input = prompt(`Le dossier mère n'existe pas. Voulez-vous le créer ?(y/n)`);
+            if(input==='y') {
+                fs.mkdir(`../${motherPath}`);
+                console.log(`Dossier mère créé.`);
+                fs.mkdir(`../${motherPath}/${DBname}`);
+                console.log(`La base de donnée ${DBname} est établie.`);
+            } else {
+                console.log('Interruption de la création de la base de donnée.');
+            }
+        } else {
+            fs.mkdir(`../${motherPath}/${DBname}`);
+            console.log(`La base de donnée ${DBname} est établie.`);
+        }
+        
+    }
+
+
 }
+
+
+
+module.exports = {LDB};
---
+++ LDB/main.js
@@ -0,0 +1,3 @@
+const LBD = require('./ldb.js');
+
+
